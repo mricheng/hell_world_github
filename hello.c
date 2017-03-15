2bbb4d8b42d28b607a1b4d19ea04e1376d5e4751
mricheng <mricheng@hotmail.com>
2017-03-15 12:10:17 +1100

    the first version of hello in Github

---
+++ hello.c
@@ -0,0 +1,15 @@
+// 
+// hello, the world in Github
+// compile by:   gcc -o hello hello.c
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+	printf("Hello, the world via Github\n");
+
+	exit(0);
+}
+ 
